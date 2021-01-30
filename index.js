476c758d3ad6b4d141ddf7624a94aa01c7b858b4
DevMehta-1 <68862426+DevMehta-1@users.noreply.github.com>
2021-01-30 13:08:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express=require('express');
 const mongoose=require('mongoose');
+const bodyParser=require('body-parser');
 const route=require('./route');
 
 mongoose.connect("mongodb+srv://Dev:Devahir012016@cluster0.pctfg.mongodb.net/Student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     ()=>{
         const app=express();
+        app.use(bodyParser.urlencoded({extended:true}));
+        app.use(express.json());
         app.use("/api",route);
-        app.listen(process.env.PORT|| 3000,()=>{
+        app.listen(process.env.PORT||3000,()=>{
             console.log("server started");
         })
     }
--- route.js
+++ route.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const student = require('./model/student');
 const Student=require('./model/student');
 const router=express.Router();
 
@@ -6,5 +7,45 @@ router.get("/students",async (req,res)=>{
     const student=await Student.find();
     res.send(student);
 });
+
+router.post("/students",async(req,res)=>{
+    const student=new Student({
+        name:req.body.name,
+        branch:req.body.branch,
+        sem:req.body.sem
+    });
+    await student.save();
+    res.send(student); 
+});
+
+router.delete("/students/:id",async(req,res)=>{
+    await Student.deleteOne({_id:req.params.id});
+    res.send("deleted");
+});
+
+router.patch("/students/:id", async (req, res) => {
+
+    try {
+
+        const student = await Student.findOne({ _id: req.params.id });
+        student.name=req.body.name;
+        student.branch=req.body.branch;
+        student.sem=req.body.sem;
+
+        await student.save();
+
+        res.send(student);
+
+    } catch {
+
+        res.status(404)
+
+        res.send({ error: "doesn't exist!" });
+
+    }
+
+});
+
 module.exports=router;
+
 
